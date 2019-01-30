ce32f66a58ee38a823377e29e12c98ccb603aadd
Dominik Zając <zajac_dominik@onet.pl>
2019-01-30 19:46:19 +0100

    First commit

---
+++ client/modules/Post/components/PostListItem/PostListItem.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router';
+import { FormattedMessage } from 'react-intl';
+
+// Import Style
+import styles from './PostListItem.css';
+
+function PostListItem(props) {
+  return (
+    <div className={styles['single-post']}>
+      <h3 className={styles['post-title']}>
+        <Link to={`/posts/${props.post.slug}-${props.post.cuid}`} >
+          {props.post.title}
+        </Link>
+      </h3>
+      <div className={styles['post-votes-container']}>
+        <p className={styles['post-votes']}>{props.post.voteCount}</p>
+      </div>
+      <div>
+        <p className={styles['author-name']}><FormattedMessage id="by" /> {props.post.name}</p>
+        <p className={styles['post-desc']}>{props.post.content}</p>
+        <p className={styles['post-action']}><a href="#" onClick={props.onDelete}><FormattedMessage id="deletePost" /></a></p>
+      </div>
+      <div className={styles['thumb-post']}>
+        <button className={styles['thumb-up-post']} onClick={props.thumbUp}>+</button>
+        <button className={styles['thumb-down-post']} onClick={props.thumbDown}>-</button>
+      </div>
+      <hr className={styles.divider} />
+    </div>
+  );
+}
+
+PostListItem.propTypes = {
+  post: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    slug: PropTypes.string.isRequired,
+    cuid: PropTypes.string.isRequired,
+  }).isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
+
+export default PostListItem;
